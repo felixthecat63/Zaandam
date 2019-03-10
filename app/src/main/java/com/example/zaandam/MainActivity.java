785dc8a315d4dcbdf22b80fe13051a6d4838be52
felixthecat63 <gofinistere@gmail.com>
2019-03-10 11:01:35 +0100

    elena -  Gestion exceptions sur l'activité MainActivity. L'application peut fonctionner en mode offline.

--- app/src/main/java/com/example/zaandam/MainActivity.java
+++ app/src/main/java/com/example/zaandam/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.zaandam;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,7 +47,21 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the getMyLocation button */
     public void getCurrentLocation(View view) {
-        Intent intent = new Intent(this, DisplayMapActivity.class);
-        startActivity(intent);
+        if (isNetworkAvailable()) {
+            Intent intent = new Intent(this, DisplayMapActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(MainActivity.this, "No internet connection detected: your location can't be retrieved but you can use the offline mode", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, GeocodingActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
